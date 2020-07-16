eca826cc5555bd96aef9ae1580ce8d583b078e99
karthi07 <itkarthi02@gmail.com>
2020-07-16 22:32:15 +0530

    add Cel and Far convert

--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ import regeneratorRuntime from 'regenerator-runtime';
 
 async function getWeather(city) {
   const celcius = '&units=metric';
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=##${celcius}`, { mode: 'cors' });
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9ea09122bccc9868a9ac88372ec1ff65`, { mode: 'cors' });
   const data = await res.json();
-  console.log(data);
+  const icon = 'http://openweathermap.org/img/wn/10d@2x.png';
   const card = document.getElementById('detailsCard');
   card.classList = 'card bg-light my-3';
-  document.getElementById('card-header').innerHTML = `${data.name}  ${data.sys.country}`;
-  document.getElementById('card-title').innerHTML = `${data.main.temp}°C   ${data.weather[0].description}`;
+  const cel = +(`${Math.round(`${data.main.temp - 273.15}e+2`)}e-2`);
+  const celDeg = `${cel} °C `;
+  const fer = +(`${Math.round(`${(data.main.temp - 273.15) * 1.8000 + 32.00}e+2`)}e-2`);
+  const ferDeg = `${fer} °F `;
+  document.getElementById('card-header').innerHTML = `${data.name} ${data.main.temp}  ${data.sys.country}`;
+  document.getElementById('card-title').innerHTML = `${celDeg} ${ferDeg}`;
   // console.log(`${data.name} temp: ${data.main.temp}`);
+  // ${data.weather[0].description}
+  // +(Math.round(num + "e+2")  + "e-2")
 }
 
 
