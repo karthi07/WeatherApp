38216e8a53a4b75d2530aa1277d462c2ac46dcf0
karthi07 <itkarthi02@gmail.com>
2020-07-17 01:26:45 +0530

    add changes for mobile responsiveness

--- src/index.js
+++ src/index.js
@@ -13,14 +13,13 @@ let ferDeg;
 const render = (data) => {
   const icon = 'http://openweathermap.org/img/wn/10d@2x.png';
   const card = document.getElementById('detailsCard');
-  card.classList = 'card bg-secondary text-white my-3';
+  card.classList = 'card bg-secondary text-white mx-auto my-3';
   const cel = +(`${Math.round(`${data.main.temp - 273.15}e+2`)}e-2`);
   celDeg = `${cel} °C `;
   const fer = +(`${Math.round(`${(data.main.temp - 273.15) * 1.8000 + 32.00}e+2`)}e-2`);
   ferDeg = `${fer} °F `;
 
   deg = isCel ? celDeg : ferDeg;
-  console.log(data);
   document.getElementById('card-header').innerHTML = `${data.name}  ${data.sys.country}`;
   document.getElementById('card-title').innerHTML = `${deg}`;
   document.getElementById('card-info').innerHTML = `${data.weather[0].description}`;
@@ -28,10 +27,10 @@ const render = (data) => {
 };
 
 async function getWeather(city) {
-  // console.log('helo');
   const celcius = '&units=metric';
   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9ea09122bccc9868a9ac88372ec1ff65`, { mode: 'cors' });
   const data = await res.json();
+  // eslint-disable-next-line no-alert
   if (data.cod === 200) { render(data); } else { alert('Enter Valid city name'); }
 }
 
